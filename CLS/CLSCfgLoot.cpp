cb3fd98dd74c2b7cd6f8ce561328c88a3141a9fb
rentiger1112 <rentiger1112@gmail.com>
2016-04-13 13:14:43 -0700

    Loot table edits
    Loot table edits

--- CLS/CLSCfgLoot.cpp
+++ CLS/CLSCfgLoot.cpp
@@ -1,10 +1,222 @@
 class CLSCfgLootTable
 {
+
+	class CUP_arifle_AK
+	{
+		itemType = "weapon";
+		items[] = {
+		{ "CUP_arifle_AK74" },
+		{ "CUP_arifle_AK74M" },
+		{ "CUP_arifle_AK74_GL" },
+		{ "CUP_arifle_AKS" },
+		{ "CUP_arifle_AKS_Gold" },
+		{ "CUP_arifle_AK74_GL_kobra" },
+		{ "CUP_arifle_AKS74" },
+		{ "CUP_arifle_AKS74_kobra" },
+		{ "CUP_arifle_AKS74_pso" },
+		{ "CUP_arifle_AKS74U" },
+		{ "CUP_arifle_AKS74UN_kobra_snds" },
+		{ "CUP_arifle_AK107" },
+		{ "CUP_arifle_AK107_kobra" },
+		{ "CUP_arifle_AK107_pso" },
+		{ "CUP_arifle_AK107_GL" },
+		{ "CUP_arifle_AK107_GL_kobra" },
+		{ "CUP_arifle_AKM" }
+		};
+	};
+
+	class CUP_arifle_CZ
+	{
+		itemType = "weapons";
+		items[] = {
+			{ "CUP_arifle_CZ805_A1" },
+			{ "CUP_arifle_CZ805_A1_Holo_Laser" },
+			{ "CUP_arifle_CZ805_A1_Aco_Laser" },
+			{ "CUP_arifle_CZ805_A1_MRCO_Laser" },
+			{ "CUP_arifle_CZ805_A1_ZDDot_Laser" },
+			{ "CUP_arifle_CZ805_GL" },
+			{ "CUP_arifle_CZ805_GL_Hamr_Laser" },
+			{ "CUP_arifle_CZ805_GL_ZDDot_Laser" },
+			{ "CUP_arifle_CZ805_A2" },
+			{ "CUP_arifle_CZ805_A2_Holo_Laser" },
+			{ "CUP_arifle_CZ805_A2_Aco_Laser" },
+			{ "CUP_arifle_CZ805_A2_ZDDot_Flashlight_Snds" },
+			{ "CUP_arifle_CZ805_B" },
+			{ "CUP_arifle_CZ805_B_GL" },
+			{ "CUP_arifle_CZ805B_GL_ACOG_Laser" }
+		};
+	};
+
+class CUP_arifle_FNFAL
+	{
+	itemType = "weapon";
+	items[] = {
+		{ "CUP_arifle_FNFAL" },
+		{ "CUP_arifle_FNFAL_railed" },
+		{ "CUP_arifle_FNFAL_ANPVS4" }
+		};
+	};
+
+	class CUP_arifle_G36
+	{
+		itemType = "weapon";
+		items[] = {
+			{ "CUP_arifle_G36A" },
+			{ "CUP_arifle_G36A_camo" },
+			{ "CUP_arifle_G36K" },
+			{ "CUP_arifle_G36K_camo" },
+			{ "CUP_arifle_G36C" },
+			{ "CUP_arifle_G36C_holo_snds" },
+			{ "CUP_arifle_G36C_camo" },
+			{ "CUP_arifle_G36C_camo_holo_snds" }
+		};
+	};
+
+	class CUP_arifle_L85A2
+	{
+		itemType = "weapon";
+		items[] = {
+			{ "CUP_arifle_L85A2" },
+			{ "CUP_arifle_L85A2_Holo_laser" },
+			{ "CUP_arifle_L85A2_SUSAT_Laser" },
+			{ "CUP_arifle_L85A2_CWS_Laser" },
+			{ "CUP_arifle_L85A2_ACOG_Laser" },
+			{ "CUP_arifle_L85A2_GL" },
+			{ "CUP_arifle_L85A2_GL_Holo_laser" },
+			{ "CUP_arifle_L85A2_GL_SUSAT_Laser" },
+			{ "CUP_arifle_L85A2_GL_ACOG_Laser" }
+			};
+	};
+
+	class CUP_arifle_m16
+	{
+		itemType = "weapon";
+		items[] = {
+			{ "CUP_arifle_M16A2" },
+			{ "CUP_arifle_M16A2_GL" },
+			{ "CUP_arifle_M16A4_Base" },
+			{ "CUP_arifle_M16A4_GL" },
+			{ "CUP_arifle_M16A4_Aim_Laser" },
+			{ "CUP_arifle_M16A4_ACOG_Laser" },
+			{ "CUP_arifle_M16A4_GL_Laser" },
+			{ "CUP_arifle_M16A4_GL_ACOG_Laser" }
+		};
+	};
+
+	class CUP_arifle_M4
+	{
+	itemType = "weapon";
+	items[] = {
+			{ "CUP_arifle_M4A1_BUIS_GL" },
+			{ "CUP_arifle_M4A1_BUIS_camo_GL" },
+			{ "CUP_arifle_M4A1" },
+			{ "CUP_arifle_M4A1_camo" },
+			{ "CUP_arifle_M4A1_black" },
+			{ "CUP_arifle_M4A3_desert" },
+			{ "CUP_arifle_M4A1_camo_Aim" },
+			{ "CUP_arifle_M4A3_desert_Aim_Flashlight" },
+			{ "CUP_arifle_M4A3_desert_GL_ACOG_Laser" },
+			{ "CUP_arifle_M4A1_Aim" },
+			{ "CUP_arifle_M4A1_camo_AIM_snds" },
+			{ "CUP_arifle_M4A1_GL_Holo_Flashlight" },
+			{ "CUP_arifle_M4A1_GL_ACOG_Flashlight" },
+			{ "CUP_arifle_M4A1_camo_GL_Holo_Flashlight" },
+			{ "CUP_arifle_M4A1_camo_GL_Holo_Flashlight_Snds" }
+		};
+	};
+
+	class CUP_arifle_VZ
+	{
+		itemType = "item";
+		items[] = {
+			{ "CUP_arifle_Sa58P" },
+			{ "CUP_arifle_Sa58V" },
+			{ "CUP_arifle_Sa58V_ACOG_Laser" },
+			{ "CUP_arifle_Sa58V_Aim_Laser" },
+			{ "CUP_arifle_Sa58RIS1" },
+			{ "CUP_arifle_Sa58RIS1_Aco_Laser" },
+			{ "CUP_arifle_Sa58RIS2" },
+			{ "CUP_arifle_Sa58RIS2_Arco_Laser" },
+			{ "CUP_arifle_Sa58RIS2_gl" },
+			{ "CUP_arifle_Sa58P_des" },
+			{ "CUP_arifle_Sa58V_camo" },
+			{ "CUP_arifle_Sa58RIS1_des" },
+			{ "CUP_arifle_Sa58RIS1_camo_Aco_Laser" },
+			{ "CUP_arifle_Sa58RIS2_camo" },
+			{ "CUP_arifle_Sa58RIS2_camo_Arco_Laser" }
+		};
+	};
+
+	class CUP_arifle_MK16
+	{
+		itemType = "weapon";
+		items[] = {
+			{ "CUP_arifle_Mk16_STD" },
+			{ "CUP_arifle_Mk16_STD_FG" },
+			{ "CUP_arifle_Mk16_STD_FG_Holo_Laser" },
+			{ "CUP_arifle_Mk16_STD_FG_LeupoldMk4CQT_Laser" },
+			{ "CUP_arifle_Mk16_STD_SFG" },
+			{ "CUP_arifle_Mk16_STD_EGLM" },
+			{ "CUP_arifle_Mk16_STD_EGLM_ACOG_Laser" },
+			{ "CUP_arifle_Mk16_STD_EGLM_ANPAS13c1_Laser_mfsup" },
+			{ "CUP_arifle_Mk16_CQC" },
+			{ "CUP_arifle_Mk16_CQC_FG" },
+			{ "CUP_arifle_Mk16_CQC_FG_Aim_Laser_snds" },
+			{ "CUP_arifle_Mk16_CQC_SFG" },
+			{ "CUP_arifle_Mk16_CQC_SFG_Holo" },
+			{ "CUP_arifle_Mk16_CQC_EGLM" },
+			{ "CUP_arifle_Mk16_CQC_EGLM_Holo_Laser_mfsup" },
+			{ "CUP_arifle_Mk16_SV" }
+		};
+	};
+
+	class CUP_arifle_MK17
+	{
+		itemType  "weapon";
+		items[] = {
+			{ "CUP_arifle_Mk17_CQC" },
+			{ "CUP_arifle_Mk17_CQC_FG" },
+			{ "CUP_arifle_Mk17_CQC_SFG" },
+			{ "CUP_arifle_Mk17_CQC_EGLM" },
+			{ "" },
+			{ "" },
+			{ "" },
+			{ "" },
+			{ "" },
+			{ "" },
+			{ "" },
+			{ "" },
+			{ "" },
+			{ "" },
+			{ "" },
+			{ "" },
+			{ "" },
+			{ "" },
+			{ "" },
+			{ "" },
+			{ "" },
+			{ "" },
+			{ "" },
+			{ "" },
+			{ "" },
+			{ "" },
+			{ "" },
+
+		}
+	}
+
 	class Items
 	{
 		itemType = "item";
 		items[] = {
-		{ "Rangefinder" },{ "NVGoggles" },{ "ItemCompass" },{ "ItemWatch" },{ "ItemRadio" },{ "ItemGPS" },{ "Binocular" },{ "Exile_Item_CanOpener" }
+		{ "Rangefinder" },
+		{ "NVGoggles" },
+		{ "ItemCompass" },
+		{ "ItemWatch" },
+		{ "ItemRadio" },
+		{ "ItemGPS" },
+		{ "Binocular" },
+		{ "Exile_Item_CanOpener" }
 		};
 	};
 	class Equipment
